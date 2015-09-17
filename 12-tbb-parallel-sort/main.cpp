b1ac77085bde6b3076f6cbd21bd2012049a9336c
Francis Giraldeau <francis.giraldeau@gmail.com>
2015-09-16 21:49:18 -0400

    Complete parallel_sort example
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- 12-tbb-parallel-sort/main.cpp
+++ 12-tbb-parallel-sort/main.cpp
@@ -1,8 +1,26 @@
-#include <QCoreApplication>
+#include <QDebug>
+#include <algorithm>
+#include "tbb/tbb.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    (void) argc; (void) argv;
 
-    return a.exec();
+    int n = 10;
+    QVector<int> data(n);
+
+    for (int i = 0; i < n; i++) {
+        data[i] = i;
+    }
+
+    std::random_shuffle(data.begin(), data.end());
+    qDebug() << "rand" << data;
+
+    tbb::parallel_sort(data.begin(), data.end());
+    qDebug() << "incr" << data;
+
+    tbb::parallel_sort(data.begin(), data.end(), std::greater<int>());
+    qDebug() << "desc" << data;
+
+    return 0;
 }
