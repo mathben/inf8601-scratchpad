db0c0cd2e408649a303892bedfad611da255023c
Francis Giraldeau <francis.giraldeau@gmail.com>
2015-10-06 16:14:21 -0400

    Adding more examples
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- 24-simd-base/main.cpp
+++ 24-simd-base/main.cpp
@@ -1,4 +1,7 @@
 #include <QDebug>
+#include <QString>
+#include <cstdio>
+#include <cassert>
 
 #include "mmx.h"
 
@@ -7,6 +10,13 @@ int main(int argc, char *argv[])
     (void) argc; (void) argv;
 
     hello();
+
+    std::printf("%X\n", add2(1, 2));
+
+    int cafebabe1 = add10_v1(0xCAFE0000, 0, 0, 0, 0, 0, 0, 0, 0, 0x0000BABE);
+    int cafebabe2 = add10_v2(0xCAFE0000, 0, 0, 0, 0, 0, 0, 0, 0, 0x0000BABE);
+    std::printf("%X\n", cafebabe1);
+    std::printf("%X\n", cafebabe2);
 
     return 0;
 }
--- 24-simd-base/mmx.h
+++ 24-simd-base/mmx.h
@@ -7,6 +7,14 @@ extern "C"
 #endif
 
 void hello(void);
+int add2(int x, int y);
+int add10_v1(int a, int b, int c, int d,
+             int e, int f, int g, int h,
+             int i, int j);
+
+int add10_v2(int a, int b, int c, int d,
+             int e, int f, int g, int h,
+             int i, int j);
 
 #ifdef __cplusplus
 }
