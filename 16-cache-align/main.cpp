60675a523e2fdc354cb833675729b5c5e184b47c
Mathieu Benoit <mathben963@gmail.com>
2015-10-22 05:48:08 -0400

    Add explication and more debug for better comprehension of 16-cache-align

--- 16-cache-align/main.cpp
+++ 16-cache-align/main.cpp
@@ -1,12 +1,15 @@
 #include <QDebug>
 #include <QString>
 
+#define NB_BIT_CACHE 6
+#define CACHE_LENGTH (1 << NB_BIT_CACHE) // 64-byte cache blocks
+
 bool is_bleeding(void *addr, size_t size)
 {
     ulong ula = (ulong) addr;
-    ulong x0 = ula % 64;
+    ulong x0 = ula % CACHE_LENGTH; // the beginning of the cache
     ulong x1 = x0 + size - 1;
-    bool bleed = (x1 >= 64);
+    bool bleed = (x1 >= CACHE_LENGTH);
     qDebug() << addr << x0 << x1 << bleed;
     return bleed;
 }
@@ -21,6 +24,8 @@ int main(int argc, char *argv[])
     long d = 4;
     long e = 5;
     long f = 6;
+    long g = 7;
+    long h = 8;
 
     is_bleeding(&a, sizeof(long));
     is_bleeding(&b, sizeof(long));
@@ -28,20 +33,28 @@ int main(int argc, char *argv[])
     is_bleeding(&d, sizeof(long));
     is_bleeding(&e, sizeof(long));
     is_bleeding(&f, sizeof(long));
+    is_bleeding(&g, sizeof(long));
+    is_bleeding(&h, sizeof(long));
 
-    char* buf = new char[512];
-    ulong ulbuf = (ulong) buf;
-    ulbuf = ulbuf & (~0 << 6);
-    long *foo = (long *) (ulbuf + (64 - 4));
+    long buf = 9;
+    ulong ulbuf = (ulong) &buf;
+    // put zero last NB_BIT_CACHE bits
+    ulbuf = ulbuf & (~0 << NB_BIT_CACHE);
+    // force wrong alignment with CACHE_LENGTH
+    long *foo = (long *) (ulbuf + (CACHE_LENGTH - sizeof(long) + 1) );
+    qDebug() << "\nreal &" << &buf << "value" << buf ;
+    qDebug() << "fake &" << QString("0x%1").arg(ulbuf, 0, 16) << "value" << *foo << "&foo " << foo;
+    qDebug() << "Expect bleed";
+    // pass pointer to wrong address alignment
     is_bleeding(foo, sizeof(long));
 
-    long __attribute__((aligned(64))) x = 42;
-    long __attribute__((aligned(64))) y = 42;
-    long __attribute__((aligned(64))) z = 42;
+    qDebug() << "\nTest force aligned";
+    long __attribute__((aligned(CACHE_LENGTH))) x = 42;
+    long __attribute__((aligned(CACHE_LENGTH))) y = 42;
+    long __attribute__((aligned(CACHE_LENGTH))) z = 42;
     is_bleeding(&x, sizeof(long));
     is_bleeding(&y, sizeof(long));
     is_bleeding(&z, sizeof(long));
 
-    delete[] buf;
     return 0;
 }
