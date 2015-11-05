e9529dc31f912a05cfa95b2e0f7f6a0b46dde57f
Francis Giraldeau <francis.giraldeau@gmail.com>
2015-11-04 21:40:12 -0500

    Ajout des commentaires
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- opencl/main.cpp
+++ opencl/main.cpp
@@ -14,51 +14,70 @@ public:
     OpenCL() { }
 
     void init() {
-        /* get the number of platforms */
+        /*
+         * En premier lieu, il faut obtenir les identifiants des
+         * plateformes disponibles. Avant, il faut allouer la mémoire
+         * requise pour contenir ces identifiants, donc il faut savoir
+         * le nombre de plateformes disponibles.
+         */
         cl_uint nrPlatforms = 0;
         clGetPlatformIDs(0, nullptr, &nrPlatforms);
 
         if (nrPlatforms == 0)
             throw std::runtime_error("No OpenCL platform");
 
-        /* get the actual platform ids */
+        /*
+         * Maintenant que le vecteur est de la bonne taille, on obtient
+         * les identifiants eux-mêmes.
+         */
         m_platforms.resize(nrPlatforms);
         clGetPlatformIDs(nrPlatforms, m_platforms.data(), nullptr);
 
-        /* devices */
+        /*
+         * Inspection des périphériques (device) pour chaque plateforme.
+         * Ceci se fait aussi en deux étapes: on obtient le nombre de device
+         * pour la plateforme, on alloue le vecteur et finalement on obtient
+         * les identifiants de périphérique.
+         */
         for(const auto id: m_platforms) {
             cl_uint nDev = 0;
             clGetDeviceIDs(id, CL_DEVICE_TYPE_ALL, 0, nullptr, &nDev);
             QVector<cl_device_id> deviceIDs(nDev);
             clGetDeviceIDs(id, CL_DEVICE_TYPE_ALL, nDev, deviceIDs.data(), nullptr);
             for (const auto dev: deviceIDs) {
-                m_devices.insert(id, dev);
+                m_devices.insert(dev, id);
             }
         }
         if (m_devices.size() == 0)
-            throw std::runtime_error("No OpenCL context");
+            throw std::runtime_error("No OpenCL device");
 
-        qDebug() << m_platforms;
-        qDebug() << m_devices;
+        qDebug() << "platforms" << m_platforms;
+        qDebug() << "devices" << m_devices;
 
     }
 
-    cl_platform_id get_platform() {
-        return m_platforms.first();
-    }
-
+    /* Par simplicité, on retourne le premier device */
     cl_device_id get_device() {
-        return m_devices[get_platform()];
+        return m_devices.keys().first();
     }
 
     QVector<cl_platform_id> m_platforms;
-    QMap<cl_platform_id, cl_device_id> m_devices;
+    QMap<cl_device_id, cl_platform_id> m_devices;
 };
 
 #define MULTI_LINE_STRING(...) #__VA_ARGS__
 class SaxpyCL {
 
-    /* the kernel string */
+    /*
+     * La chaine de caractère du kernel. Cette chaine représente
+     * le code qui est compilé pour le périphérique OpenCL. On utilise
+     * une macro pour éviter les guillemets, mais dans le laboratoire,
+     * on ajoute la chaine au fichier objet après avoir vérifié qu'il
+     * compile (en utilisant openclcc).
+     *
+     * __kernel : indique une fonction que l'on peut appeler
+     * __global : indique un tampon créé avec clCreateBuffer()
+     */
     const char *kernel_text = MULTI_LINE_STRING(
         __kernel void SAXPY (__global float* x,
                              __global float* y,
@@ -77,39 +96,76 @@ public:
 
     void init(cl_device_id dev) {
         cl_int ret;
+        /* Création du contexte (regroupe toutes les opérations reliées) */
         m_ctx       = clCreateContext(nullptr, 1, &dev, nullptr, nullptr, &ret);
+
+        /* Création du programme (mais ne le compile pas) */
         m_prog      = clCreateProgramWithSource(m_ctx, 1, &kernel_text, nullptr, nullptr);
+
+        /* Compile le programme */
         ret         = clBuildProgram(m_prog, 0, nullptr, "", nullptr, nullptr);
+
+        /* obtient une référence du kernel (on doit spécifier un nom de fonction annotée avec __kernel) */
         m_kernel    = clCreateKernel(m_prog, "SAXPY", &ret);
+
+        /* Création de la file d'attente de commande (les commandes sont exécutées dans l'ordre FIFO) */
         m_queue     = clCreateCommandQueue(m_ctx, dev, 0, &ret);
     }
 
     void compute(float a, QVector<float> &x, QVector<float> &y, QVector<float> &z) {
         cl_int ret;
+
+        /* la taille en octets des tampons, requis pour allouer les tampons sur le périphérique */
         size_t size = x.size() * sizeof(float);
+
+        /*
+         * work_size est un tableau des tailles pour chaque dimensions. Ici, les
+         * vecteurs sont des donnée 1D.
+         *
+         * Si on travaille en 2D, alors le tableau a deux entiers: {x, y}
+         * Le kernel est appelé pour chaque élément de dimension, et l'index
+         * est retourné par get_global_id(dimension). get_global_id(0) retourne
+         * l'indexe x et get_global_id(1) retourne l'indexe y.
+         */
         size_t work_size[] = { size };
 
+        /*
+         * Création des tampons sur la carte. Les opérations CL_MEM_READ/CL_MEM_WRITE
+         * sont du point de vue du kernel. Ici, le kernel lit depuis x et y, mais
+         * écrit dans z.
+         */
         cl_mem cl_x = clCreateBuffer(m_ctx, CL_MEM_READ_ONLY, size, nullptr, &ret);
         cl_mem cl_y = clCreateBuffer(m_ctx, CL_MEM_READ_ONLY, size, nullptr, &ret);
         cl_mem cl_z = clCreateBuffer(m_ctx, CL_MEM_WRITE_ONLY, size, nullptr, &ret);
 
+        /*
+         * Copie des données de l'ordinateur vers la carte. Les opérations Read/Write sont
+         * du point de vue de l'ordinateur hôte. On écrit vers la carte les données des
+         * vecteurs x et y.
+         */
         clEnqueueWriteBuffer(m_queue, cl_x, CL_FALSE, 0, size, x.data(), 0, nullptr, nullptr);
         clEnqueueWriteBuffer(m_queue, cl_y, CL_FALSE, 0, size, y.data(), 0, nullptr, nullptr);
 
+        /* Définition des arguments en ordre */
         clSetKernelArg(m_kernel, 0, sizeof(cl_mem), &cl_x);
         clSetKernelArg(m_kernel, 1, sizeof(cl_mem), &cl_y);
         clSetKernelArg(m_kernel, 2, sizeof(cl_mem), &cl_z);
         clSetKernelArg(m_kernel, 3, sizeof(float), &a);
 
+        /* Lancement du calcul */
         clEnqueueNDRangeKernel(m_queue, m_kernel, 1, nullptr, work_size, nullptr, 0, nullptr, nullptr);
+
+        /* Récupération du résultat (copie de la carte vers l'hôte) */
         clEnqueueReadBuffer(m_queue, cl_z, CL_TRUE, 0, size, z.data(), 0, nullptr, nullptr);
 
+        /* Libération des tampons sur la carte */
         clReleaseMemObject(cl_x);
         clReleaseMemObject(cl_y);
         clReleaseMemObject(cl_z);
     }
 
     void close() {
+        clFinish(m_queue);
         clReleaseCommandQueue(m_queue);
         clReleaseContext(m_ctx);
     }
